90223903e3c646fccbab34a314b71acd054ebf5f
Gilberto Junior <giljrsantos@gmail.com>
2025-03-26 19:44:55 -0300

    adiciona documentação ao serviço de status de rede e inclui imagem de tela

--- src/app/core/service/network-status.service.ts
+++ src/app/core/service/network-status.service.ts
@@ -15,16 +15,31 @@ export class NetworkStatusService {
     window.addEventListener('offline', () => this.updateOnlineStatus(false));
   }
 
+  /**
+   * Updates the online status and emits a new value to all subscribers.
+   * @param status the new online status
+   */
   private updateOnlineStatus(status: boolean) {
     this.onlineStatus.next(status);
   }
 
   // Retorna o estado da conexão como Observable
+  /**
+   * Retrieves an observable that emits the current network status.
+   *
+   * @returns {Observable<boolean>} An observable that emits `true` when the network is online
+   * and `false` when it is offline.
+   */
   getNetworkStatus(): Observable<boolean>{
     return this.onlineStatus.asObservable();
   }
 
   // Metodo para verificar se a conexão está online
+  /**
+   * Checks the current online status of the application.
+   *
+   * @returns {boolean} `true` if the application is online, otherwise `false`.
+   */
   isOnline(): boolean {
     return this.onlineStatus.value;
   }
